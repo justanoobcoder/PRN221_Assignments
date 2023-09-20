30c85cfa14a0b6524c01afbd19300bb43aaf932c
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-09-20 09:55:24 +0700

    added converters

---
+++ NguyenHongHiepWPF/Converters/BirthdayConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NguyenHongHiepWPF.Converters;
+
+public class BirthdayConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime birthday)
+        {
+            return birthday.ToString("dd/MM/yyyy");
+        }
+        return "";
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string birthday)
+        {
+            return DateTime.ParseExact(birthday, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+        }
+        return null;
+    }
+}
---
+++ NguyenHongHiepWPF/Converters/StatusConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NguyenHongHiepWPF.Converters;
+
+public class StatusConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        byte status = (byte)value;
+        return status switch
+        {
+            1 => "Active",
+            0 => "Deleted",
+            _ => "Unknown",
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string status = (string)value;
+        return status switch
+        {
+            "Active" => 1,
+            "Deleted" => 0,
+            _ => 2,
+        };
+    }
+}
