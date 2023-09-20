db4bc3add5d824c66ace9e4d3756be78acc13e50
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-09-20 09:54:16 +0700

    added validation project

---
+++ Validation/StringValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Validation;
+
+public static class StringValidation
+{
+    public static bool IsEmail(this string email)
+    {
+        Regex validateEmailRegex = new Regex("^\\S+@\\S+\\.\\S+$");
+        return validateEmailRegex.IsMatch(email);
+    }
+
+    public static bool IsPhoneNumber(this string phone)
+    {
+        Regex validatePhoneNumberRegex = new Regex("^\\+?[1-9][0-9]{7,14}$");
+        return validatePhoneNumberRegex.IsMatch(phone);
+    }
+
+    public static bool IsBlank(this string str)
+    {
+        return string.IsNullOrEmpty(str.Trim());
+    }
+}
