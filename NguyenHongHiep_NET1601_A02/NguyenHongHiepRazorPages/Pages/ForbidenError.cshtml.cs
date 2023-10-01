3809a85d1dd5c5a75c69dae3b7a7697e80288d72
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-10-01 16:21:12 +0700

    added forbiden error page

---
+++ NguyenHongHiep_NET1601_A02/NguyenHongHiepRazorPages/Pages/ForbidenError.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NguyenHongHiepRazorPages.Pages
+{
+    public class ForbidenErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
