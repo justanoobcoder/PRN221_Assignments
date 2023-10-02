5a3e15552dbea9bc1db5ed89b29b26a5b5d38cb9
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-10-02 08:09:03 +0700

    done register

--- NguyenHongHiep_NET1601_A02/NguyenHongHiepRazorPages/Pages/Register.cshtml.cs
+++ NguyenHongHiep_NET1601_A02/NguyenHongHiepRazorPages/Pages/Register.cshtml.cs
@@ -1,20 +1,70 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NguyenHongHiepRazorPages.Admin;
 using NguyenHongHiepRazorPages.Models;
+using NguyenHongHiepRazorPages.Utils;
+using Repositories;
 
 namespace NguyenHongHiepRazorPages.Pages;
 
 [BindProperties]
 public class RegisterModel : PageModel
 {
+    private readonly ICustomerRepository _customerRepository;
+
+    public RegisterModel(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
     public RegisterCustomerModel Customer { get; set; } = default!;
 
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        var currentUser = HttpContext.Session.GetObjectFromJson<CurrentUser>(Constants.Contants.CurrentUserKey);
+        if (currentUser != null)
+        {
+            if (currentUser.IsAdmin)
+                return RedirectToPage("/Admin/Index");
+            else
+                return RedirectToPage("/Customer/Index");
+        }
+        return Page();
     }
 
     public IActionResult OnPost()
     {
+        if (_customerRepository.ExistsByTelephone(Customer.Telephone))
+        {
+            ModelState.AddModelError("Customer.Telephone", "Phone number already exists");
+        }
+        if (Customer.Email == AdminAccount.Email || _customerRepository.ExistsByEmail(Customer.Email))
+        {
+            ModelState.AddModelError("Customer.Email", "Email already exists");
+        }
+
+        if (ModelState.IsValid)
+        {
+            var customer = new BusinessObjects.Customer
+            {
+                CustomerName = Customer.Name.Trim(),
+                Telephone = Customer.Telephone,
+                Email = Customer.Email,
+                Password = Customer.Password,
+                CustomerBirthday = Customer.Birthday,
+                CustomerStatus = 1,
+            };
+            try
+            {
+                _customerRepository.Insert(customer);
+                TempData["Success"] = "Register successfully!";
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = $"Register failed! {ex.Message}";
+            }
+        }
+
         return Page();
     }
 }
