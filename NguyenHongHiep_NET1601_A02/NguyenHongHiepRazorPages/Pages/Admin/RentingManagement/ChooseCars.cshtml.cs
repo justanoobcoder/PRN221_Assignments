c450fb42d935e4716ebc456403556b2efe9bd60f
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-10-09 16:58:22 +0700

    done create renting transaction

--- NguyenHongHiep_NET1601_A02/NguyenHongHiepRazorPages/Pages/Admin/RentingManagement/ChooseCars.cshtml.cs
+++ NguyenHongHiep_NET1601_A02/NguyenHongHiepRazorPages/Pages/Admin/RentingManagement/ChooseCars.cshtml.cs
@@ -13,11 +13,13 @@ public class ChooseCarsModel : PageModel
 {
     private readonly ICarRepository _carRepository;
     private readonly ICustomerRepository _customerRepository;
+    private readonly IRentingRepository _rentingRepository;
 
-    public ChooseCarsModel(ICarRepository carRepository, ICustomerRepository customerRepository)
+    public ChooseCarsModel(ICarRepository carRepository, ICustomerRepository customerRepository, IRentingRepository rentingRepository)
     {
         _carRepository = carRepository;
         _customerRepository = customerRepository;
+        _rentingRepository = rentingRepository;
     }
 
     public Customer Customer { get; set; } = default!;
@@ -25,6 +27,7 @@ public class ChooseCarsModel : PageModel
     public DateTime StartDate { get; set; } = DateTime.Now;
     public DateTime EndDate { get; set; } = DateTime.Now;
     public List<RentingDetailModel> RentingCars { get; set; } = new();
+    public string? Error { get; set; } = default!;
 
     public IActionResult OnGet()
     {
@@ -56,6 +59,22 @@ public class ChooseCarsModel : PageModel
             return NotFound();
         if (StartDate > EndDate)
             return BadRequest();
+        if (!_rentingRepository.CanCarBeRented(car.CarId, StartDate, EndDate))
+        {
+            var c = _customerRepository.GetById(transaction.CustomerId);
+            if (c == null || c.CustomerStatus == 0)
+                return NotFound();
+            Customer = c;
+
+            var carIds = transaction.Details.Select(d => d.CarId);
+            var cars = _carRepository.GetAll()
+                    .Where(c => c.CarStatus != 0 && !carIds.Contains(c.CarId))
+                    .ToList();
+            ViewData["CarId"] = new SelectList(cars, "CarId", "CarName");
+            RentingCars = transaction.Details;
+            ModelState.AddModelError(nameof(Error), "This car is not available in this period");
+            return Page();
+        }
         TimeSpan difference = EndDate - StartDate;
         int numberOfDays = difference.Days + 1;
         transaction.Details.Add(new RentingDetailModel
@@ -84,5 +103,37 @@ public class ChooseCarsModel : PageModel
         transaction.Details.Remove(detail);
         SessionHelper.SetObjectAsJson(HttpContext.Session, Constants.Contants.TransactionKey, transaction);
         return RedirectToPage("./ChooseCars");
+    }
+
+    public IActionResult OnPostRent()
+    {
+        TransactionModel? transaction = SessionHelper.GetObjectFromJson<TransactionModel>(HttpContext.Session, Constants.Contants.TransactionKey);
+        if (transaction == null)
+            return BadRequest();
+        if (transaction.Details.Count == 0)
+            return BadRequest();
+        RentingTransaction rentingTransaction = new()
+        {
+            CustomerId = transaction.CustomerId,
+            RentingDate = DateTime.Now,
+            TotalPrice = transaction.Details.Sum(d => d.Price),
+            RentingStatus = 1,
+        };
+        foreach (var detail in transaction.Details)
+        {
+            var car = _carRepository.GetById(detail.CarId);
+            if (car == null || car.CarStatus == 0)
+                return NotFound();
+            rentingTransaction.RentingDetails.Add(new RentingDetail
+            {
+                CarId = detail.CarId,
+                StartDate = detail.StartDate,
+                EndDate = detail.EndDate,
+                Price = car.CarRentingPricePerDay,
+            });
+        }
+        _rentingRepository.CreateTransaction(rentingTransaction);
+        HttpContext.Session.Remove(Constants.Contants.TransactionKey);
+        return RedirectToPage("./Index");
     }
 }
